7f8cb3b91b4b3a6147d9b1621867c4c60e344ae1
Vipul Vaishnav <007vipulrank1@gmail.com>
2022-10-09 14:17:27 +0530

    Setup Project

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <React.Fragment>
+      <div className="font-default">Layout</div>
+    </React.Fragment>
+  )
+}
+
+export default Layout
